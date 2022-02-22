8d911fbe7466897be0884600562dc45d867aaec8
AlejandroG10 <alejandrogilromero@fp.iesromerovargas.com>
2022-02-22 11:32:16 +0100

    Añade formulario para crear cuentas de usuario

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (
+    CreateView, UpdateView, DeleteView)                # new
+from django.urls import reverse_lazy                   # new
+
 from .models import Post
 
-
-# Create your views here.
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
@@ -11,3 +13,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
